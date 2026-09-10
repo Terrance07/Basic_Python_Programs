6f7803a83e0f81a3620db72a020a0e4a50ede9ec
Terrance Boniface <104243139+Terrance07@users.noreply.github.com>
2026-09-10 10:38:44 +0530

    Add functionality to count even and odd digits
    Count the number of even and odd digits in a given number.

---
+++ Count_Even_Odd.py
@@ -0,0 +1,12 @@
+n = int(input("Enter a Number:"))
+even = 0
+odd = 0
+while n > 0:
+  digit = n % 10
+  if n % 2 == 0:
+    even = even + 1
+  else:
+    odd = odd + 1
+  n = n // 10
+print(even)
+print(odd)

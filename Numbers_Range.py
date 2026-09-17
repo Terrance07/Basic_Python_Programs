d616ae95c2c5e0ebdd177ea04eeae220ad11d17a
Terrance Boniface <104243139+Terrance07@users.noreply.github.com>
2026-09-17 15:22:31 +0530

    Add function to print numbers in a range

---
+++ Numbers_Range.py
@@ -0,0 +1,6 @@
+start = int(input("Enter starting number: "))
+end = int(input("Enter ending number: "))
+def print_numbers(start, end):
+    for i in range(start, end + 1):
+        print(i)
+print_numbers(start, end)
